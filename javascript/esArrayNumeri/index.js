4f335b6d7cecd18448dfd45640a879c853f609db
Giuliazeolla <giuliazeolla23@gmail.com>
2024-08-22 23:24:02 +0200

    added sum of numbers

--- javascript/esArrayNumeri/index.js
+++ javascript/esArrayNumeri/index.js
@@ -1,2 +1,9 @@
-let numbers = [44, 26, 13, 17];
+const numbers = [44, 26, 13, 17];
 console.log(numbers);
+
+let sum = 0;
+
+for(let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+console.log(sum);
