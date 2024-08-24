63b2bd2766f38911f5e48ce8cda1b8441671324d
Giuliazeolla <giuliazeolla23@gmail.com>
2024-08-24 13:25:40 +0200

    fix word.length

--- javascript/esPrompt/index.js
+++ javascript/esPrompt/index.js
@@ -1,7 +1,7 @@
-let word = prompt('Inserisci una parola');
+let word;
 console.log(word.length);
 
 do {
     word = prompt('Inserisci una parola');
-} while (word.length >= 5);
+} while (word.length <= 5);
 console.log(word);
