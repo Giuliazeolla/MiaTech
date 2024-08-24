2deb99db6384c5f692365c45bf90f7ead99fdbce
Giuliazeolla <giuliazeolla23@gmail.com>
2024-08-24 14:42:56 +0200

    added words and totaleSpese

---
+++ javascript/esReduceESort/index.js
@@ -0,0 +1,10 @@
+const spese = [100, 200, 150, 50, 300];
+
+const totaleSpese = spese.reduce((totale, spese) => totale + spese);
+
+const words = ["banana", "apple", "cherry", "date"];
+
+words.sort();
+
+console.log(totaleSpese);
+console.log(words);
