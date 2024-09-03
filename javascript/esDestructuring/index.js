dcada1e12c08d713fcaf85b5f186f7c967044697
Giuliazeolla <giuliazeolla23@gmail.com>
2024-09-03 17:00:49 +0200

    added destructuring

---
+++ javascript/esDestructuring/index.js
@@ -0,0 +1,9 @@
+const array =  [3, 5, 7];
+
+const [primo = 0, secondo = 0, terzo = 0, quarto = 0, quinto = 0] = array;
+
+console.log(primo);
+console.log(secondo);
+console.log(terzo);
+console.log(quarto);
+console.log(quinto);
