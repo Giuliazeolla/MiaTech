5a0a14e623711dd163143874e16be7589d9ccfe5
Giuliazeolla <giuliazeolla23@gmail.com>
2024-09-09 14:59:19 +0200

    added sting

---
+++ javascript/esStringaMultilinea/index.js
@@ -0,0 +1,13 @@
+const nome = "Mario";
+const cognome = "Rossi";
+const età = 30;
+const città = "Roma";
+
+const infoPersona = `
+Nome: ${nome}
+Cognome: ${cognome}
+Età: ${età}
+Città: ${città}
+`;
+
+console.log(infoPersona);
