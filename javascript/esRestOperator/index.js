8a9cbc8742668a77b89ffb81c7590ec0c7f4cbc9
Giuliazeolla <giuliazeolla23@gmail.com>
2024-09-09 14:42:14 +0200

    added rest operator for object

--- javascript/esRestOperator/index.js
+++ javascript/esRestOperator/index.js
@@ -5,3 +5,17 @@ const [primo, secondo, ...resto] = numeri;
 console.log(primo); 
 console.log(secondo);
 console.log(resto);
+
+
+const persona = {
+    nome: "Giulia",
+    cognome: "Zeolla",
+    età: 23,
+    professione: "Cassiera",
+  };
+  
+  const { nome, età, ...altreProprietà } = persona;
+  
+  console.log(nome);   
+  console.log(età);    
+  console.log(altreProprietà);
