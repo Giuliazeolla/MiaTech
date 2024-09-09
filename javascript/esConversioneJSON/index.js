c9d7825feaf36a2fd4c6c963e4663855f40eb72a
Giuliazeolla <giuliazeolla23@gmail.com>
2024-09-09 14:47:44 +0200

    added JSON parse

--- javascript/esConversioneJSON/index.js
+++ javascript/esConversioneJSON/index.js
@@ -9,3 +9,10 @@ const persona = {
   
   console.log(jsonString);
   
+
+
+  const jsonStringa = '{"nome":"Giulia","cognome":"Zeolla","età":23,"città":"Benevento"}';
+
+  const person = JSON.parse(jsonStringa);
+  console.log(person);
+  
