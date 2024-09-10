6b9eda624aac7fcfe5e3fbb0f4b203749311fae3
Giuliazeolla <giuliazeolla23@gmail.com>
2024-09-10 09:12:11 +0200

    added clearInterval

--- javascript/esSetTimeout/index.js
+++ javascript/esSetTimeout/index.js
@@ -1,14 +1,25 @@
 function stampaMessaggio() {
     console.log("Questo messaggio viene stampato dopo 3 secondi.");
-  }
-  
-  setTimeout(stampaMessaggio, 3000);
-  
+}
+
+setTimeout(stampaMessaggio, 3000);
 
 
-  function stampaMessaggio() {
+
+/*function stampaMessaggio() {
     console.log('Questo messaggio viene stampato ogni 2 secondi.');
+}
+
+setInterval(stampaMessaggio, 2000);*/
+
+
+function stampaMessaggio() {
+    console.log("Questo messaggio viene stampato ogni secondo.");
   }
   
-  setInterval(stampaMessaggio, 2000);
+  const intervallo = setInterval(stampaMessaggio, 1000);
   
+  setTimeout(() => {
+    clearInterval(intervallo);
+    console.log("L'intervallo è stato interrotto.");
+  }, 5000);
