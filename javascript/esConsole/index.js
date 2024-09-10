db4636b53ba700025c9d00df82f8044f95b9ed5d
Giuliazeolla <giuliazeolla23@gmail.com>
2024-09-10 06:46:41 +0200

    added console.log

---
+++ javascript/esConsole/index.js
@@ -0,0 +1,13 @@
+const numero = 42;
+const stringa = "Ciao, mondo!";
+const array = [1, 2, 3, 4, 5];
+const oggetto = {
+  nome: "Mario",
+  cognome: "Rossi",
+  età: 30
+};
+
+console.log(numero);
+console.log(stringa);
+console.log(array);
+console.log(oggetto);
