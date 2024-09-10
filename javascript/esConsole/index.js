4d6d83f34fc516aa1d3d65ddb6656a5ebcfe6f26
Giuliazeolla <giuliazeolla23@gmail.com>
2024-09-10 06:52:05 +0200

    added console.error and console.warm

--- javascript/esConsole/index.js
+++ javascript/esConsole/index.js
@@ -11,3 +11,14 @@ console.log(numero);
 console.log(stringa);
 console.log(array);
 console.log(oggetto);
+
+
+const stato = "errore";
+
+if (stato === "errore") {
+  console.error("Si è verificato un errore!");
+} else if (stato === "avviso") {
+  console.warn("Attenzione: questo è un avviso!");
+} else {
+  console.log("Tutto sembra andare bene.");
+}
