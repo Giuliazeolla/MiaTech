c8eeee19e9a439220de4c575132b381ccf88f091
Giuliazeolla <giuliazeolla23@gmail.com>
2024-10-10 21:58:19 +0200

    added handleClick+

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -3,8 +3,15 @@ import React, { useState } from "react";
 const Counter = () => {
   const [counter, setCounter] = useState(0)
 
+  const handleClick = () => {
+    setCounter((_counter) => {
+    return _counter + 1;
+  })};
+
   return (
-    <div>Count: {counter}</div>
+    <div>Count: {counter}
+    <button onClick={handleClick}>ADD</button>
+    </div>
   )
 }
 export default Counter;
