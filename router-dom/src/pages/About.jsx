8e3c70aa7a71fcaebd9ca89ec77c9f102832de0f
Giuliazeolla <giuliazeolla23@gmail.com>
2024-10-28 12:08:26 +0100

    added button

--- router-dom/src/pages/About.jsx
+++ router-dom/src/pages/About.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const About = () => {
+    const navigate = useNavigate();
+    const goToHome = () => {
+        navigate('/');
+    }
+
     return (
         <div>
             <h1>About</h1>
             <p>Questa applicazione è un semplice gestore dei to-do</p>
+            <button onClick={goToHome}>Torna alla Home</button>
         </div>
     )
 };
