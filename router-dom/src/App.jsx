0bb41dfe5bbf22f9691c1437b7cf786240ed1503
Giuliazeolla <giuliazeolla23@gmail.com>
2024-10-28 12:04:12 +0100

    added Outlet

--- router-dom/src/App.jsx
+++ router-dom/src/App.jsx
@@ -2,17 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Layout from "./components/Layout";
 
 const App = () => {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
       <Routes>
-        <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Layout />}>
+        <Route index element={<Home />}/>
         <Route path="/about" element={<About />}/>
+        </Route>
       </Routes>
     </Router>
   )
