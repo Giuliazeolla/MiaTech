416d0f7a10fc396e5e5c1e89efc3c4579e77197d
Giuliazeolla <giuliazeolla23@gmail.com>
2024-11-11 19:05:06 +0100

    added TodoWithMetadata

--- todo-list/src/types.ts
+++ todo-list/src/types.ts
@@ -3,7 +3,10 @@ export interface Todo {
     title: string;
     completed: boolean;
     userId?: number;
-    metadata?: any;
+}
+
+interface TodoWithMetadata extends Todo {
+    metadata: any;
 }
 
 export interface User {
