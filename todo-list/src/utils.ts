86777e61c7a53aa0a962c83d86db59135c2f18f7
Giuliazeolla <giuliazeolla23@gmail.com>
2024-11-23 18:06:37 +0100

    fidex filterFn

--- todo-list/src/utils.ts
+++ todo-list/src/utils.ts
@@ -1,8 +1,13 @@
 import { Todo } from "./types";
 
-export function filterTodos(todos: Todo[], filterFn: (todo: Todo) => boolean): Todo[] {
-    return todos.filter(filterFn);
+export function filterTodos<T extends Todo>(
+    todos: T[],
+    filterFn: (todo: T) => boolean
+): T[] {
+    return todos.filter(filterFn)
 }
+
+
 
 export type PartialTodo = Partial<Todo>;
 
